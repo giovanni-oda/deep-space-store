9a615af80ad096a94c4fa32b6261033e5991d76c
Giovanni Oda <giovanni_oda@yahoo.com.br>
2023-02-12 17:03:50 -0300

    fix(components): appFooter - add year

--- src/components/appFooter.vue
+++ src/components/appFooter.vue
@@ -1,9 +1,7 @@
 <template>
   <section id="footer" class="pt-8 bg-primary">
     <v-container class="fill-height">
-      <p>
-        © All rights reserved. Made with ❤️ by Giovanni Oda to YouShop, inc.
-      </p>
+      <p>2023. © All rights reserved. Made Giovanni Oda to YouShop, inc.</p>
     </v-container>
   </section>
 </template>
